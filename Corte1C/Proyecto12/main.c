bb5a61e3f9db2de08af685151538cae6da82ccc8
JdMonDa12 <josedavidmoncadadiaz@gmail.com>
2025-10-07 19:05:50 -0500

    Add files via upload

---
+++ Corte1C/Proyecto12/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	//Se declara q el array es de 4 numeros
+	
+	int myNumbers[4];
+	
+	// Se declara cada valor de cada numero del array
+	
+	myNumbers[0] = 25;
+	
+	myNumbers[1] = 50;
+	
+	myNumbers[2] = 75;
+	
+	myNumbers[3] = 100;
+	
+	
+	return 0;
+}
