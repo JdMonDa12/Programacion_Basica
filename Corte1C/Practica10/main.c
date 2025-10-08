c845f09ddcf1390127bbcaa69e0966adf76aaa7c
JdMonDa12 <josedavidmoncadadiaz@gmail.com>
2025-10-07 19:03:20 -0500

    Add files via upload

---
+++ Corte1C/Practica10/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	int myNumbers[] = {25, 50, 75, 100};
+	
+	printf("%d", myNumbers[0]); // empieza a contar desde 0: primer elemento = 0, segundo elemento = 1, tercer elemento = 2...
+	
+	return 0;
+}
