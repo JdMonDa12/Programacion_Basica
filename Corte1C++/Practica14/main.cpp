7984cddc561aca61a47e4c9e8b4e807b08c5b0ea
JdMonDa12 <josedavidmoncadadiaz@gmail.com>
2025-10-07 19:11:36 -0500

    Add files via upload

---
+++ Corte1C++/Practica14/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+	
+	int myNumbers[] = {10, 25, 50, 75, 100}; // se declara el Array
+	
+	int length = sizeof(myNumbers) / sizeof(myNumbers[0]); // formula para hallar la cantidad de numeros en el Array
+
+	printf("%d", length);  // Length = Cantidad de elementos del Array
+	
+	return 0;
+}
