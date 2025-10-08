132542a035933097ff4433be26aa763285c130ef
JdMonDa12 <josedavidmoncadadiaz@gmail.com>
2025-10-07 19:10:53 -0500

    Add files via upload

---
+++ Corte1C++/Practica12/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+	
+	//Se declara q el array es de 4 numeros
+	
+	int myNumbers[4];
+	
+	// Se declara cada valor de cada numero del array
+	
+	myNumbers[0] = 25;
+	
+	myNumbers[1] = 50;
+	
+	myNumbers[2] = 75;
+	
+	myNumbers[3] = 100;
+	
+	
+	return 0;
+}
