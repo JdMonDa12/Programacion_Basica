616e3a2838503fe705dfb1e215384dcb4ef78397
JdMonDa12 <josedavidmoncadadiaz@gmail.com>
2025-10-09 13:26:54 -0500

    Add files via upload

---
+++ Corte1C/Practica16/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	int myNumbers[] = {25, 50, 75, 100}; // valores de la cadena establecidos como enteros
+	
+	int length = sizeof(myNumbers) / sizeof(myNumbers[0]); // calcular la longitud de la cadena
+	
+	int i;
+
+	for (i = 0; i < length; i++) { // usar la longitud de la cadena como medidor de la cantidad de valores establecidos dentro de la cadena
+		
+  		printf("%d\n", myNumbers[i]);
+  	
+}
+	
+	return 0;
+	
+}
