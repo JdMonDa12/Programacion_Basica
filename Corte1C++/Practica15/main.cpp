eb94cac2904f31882ceb0d733ca98fb26375fc2c
JdMonDa12 <josedavidmoncadadiaz@gmail.com>
2025-10-09 13:34:05 -0500

    Add files via upload

---
+++ Corte1C++/Practica15/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+	
+	int myNumbers[] = {25, 50, 75, 100}; // valores de la cadena establecidos como enteros
+	
+	int i; // establecer i como entero
+	
+	for (i = 0; i < 4; i++) { // loop para imprimir cada valor de la cadena. i++ se encarga de cambiar el valor de i para imprimir el siguiente valr
+		
+	  printf("%d\n", myNumbers[i]); //imprime con espacio
+	  
+	}
+	
+	return 0;
+	
+}
